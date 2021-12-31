a5840cfa5cffba9ad0b0cecbb240643544fa78c5
Neetu <93900988+Neetu-12@users.noreply.github.com>
2021-12-31 12:43:07 +0530

    Add files via upload

---
+++ task5.py
@@ -0,0 +1,12 @@
+import json
+from task1 import scrap_top_list
+from task4 import scrap_movie_details
+movie=scrap_top_list()
+def get_movies_details(movie):
+        details=[]
+        for i in movie:
+                details.append(scrap_movie_details(i["Url"]))
+        with open("Task5.json","w+") as file:
+                json.dump(details,file,indent=4)
+                return details
+movie_detail=get_movies_details(movie[:10])
