03e157287bef2b89da39dfc9f95667a7a8e1ad1e
Neetu <93900988+Neetu-12@users.noreply.github.com>
2022-01-01 11:12:46 +0530

    Update task7.py

--- task7.py
+++ task7.py
@@ -21,25 +21,3 @@ def analyse_movies_directors(data):
         json.dump(l,file,indent=4)
         return l
 print(analyse_movies_directors(data))
-
-
-# import json
-# with open("task5.json","r") as file:
-#     data=json.load(file)
-
-# def analyse_movies_language(data):
-#     r={}
-#     for i in data:
-#         if "Language" in i:
-#             language=i["Language"]
-#             if language not in r:
-#                 language=i["Language"]
-#                 r[language]=1
-#             else:
-#                 r[language]+=1
-#         else:
-#             continue
-#     with open("task6.json","w+") as file:
-#         json.dump(r,file,indent=4)
-#         return r
-# print(analyse_movies_language(data))
